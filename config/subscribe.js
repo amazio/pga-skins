123e4cc4c216183795aa8e61128d39899a9b4700
jim-clark <email@jim-clark.com>
2019-11-11 06:35:58 -0800

    Test subscribe

--- config/subscribe.js
+++ config/subscribe.js
@@ -5,6 +5,10 @@ module.exports = {
 };
 
 function subscribe() {
+
+
+console.log('Going to subscribe at: ' + process.env.SUBSCRIBE_POST_URL)
+
   fetch(
     process.env.SUBSCRIBE_POST_URL,
     {
@@ -17,5 +21,6 @@ function subscribe() {
     }
   )
   .then(res => res.json())
-  .then(json => console.log(json));
+  .then(json => console.log(json))
+  .catch(err => console.log(err));
 } 
