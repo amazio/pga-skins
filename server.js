3dc5acab6461fc6e377715974474e897b4878cbc
jim-clark <email@jim-clark.com>
2020-01-28 16:30:09 -0800

    Create React app and set up SPA

--- server.js
+++ server.js
@@ -1,44 +1,27 @@
-var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var logger = require('morgan');
+const favicon = require('serve-favicon');
 
 // require('dotenv').config();  // Being required in www
 
 require('./config/database');
 require('./config/subscribe').subscribe();
 
-var indexRouter = require('./routes/index');
 var holesRouter = require('./routes/api/holes');
 
 var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
-
 app.use(logger('dev'));
 app.use(express.json({limit: '200kb'}));
 app.use(express.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(favicon(path.join(__dirname, 'build', 'favicon.ico')));
+app.use(express.static(path.join(__dirname, 'build')));
 
-app.use('/', indexRouter);
 app.use('/api/holes', holesRouter);
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+app.get('/*', function(req, res) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
 module.exports = app;
