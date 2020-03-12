a0c815a87da97e6057e1492b3c326e17da5edbf7
jim-clark <email@jim-clark.com>
2020-03-12 05:59:13 -0700

    Simplify BottomNav logic

--- src/components/BottomNav/BottomNav.jsx
+++ src/components/BottomNav/BottomNav.jsx
@@ -1,40 +1,15 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import { useHistory, useLocation } from 'react-router-dom';
 import { BottomNavigation, BottomNavigationAction } from '@material-ui/core';
 import { Settings, GolfCourse } from '@material-ui/icons';
-import { set } from 'mongoose';
 
 export default function BottomNav() {
   const history = useHistory();
-  const [selected, setSelected] = useState('');
   let {pathname} = useLocation();
-
-  useEffect(function() {
-    if (pathname === '/') {
-      setSelected('matches');
-    } else if (pathname === '/settings') {
-      setSelected('settings');
-    } else {
-      // Don't select Matches or Settings icon
-      setSelected('');
-    }
-  }, [pathname]);
-
-  function handleChangeRoute(e, newRoute) {
-    switch (newRoute) {
-      case 'matches':
-        history.push('/');
-        break;
-      case 'settings':
-        history.push('/settings');
-        break;
-    }
-  }
-
   return (
-    <BottomNavigation value={selected} onChange={handleChangeRoute} showLabels>
-      <BottomNavigationAction value='matches' label='Matches' icon={<GolfCourse />} />
-      <BottomNavigationAction value='settings' label='Settings' icon={<Settings />} />
+    <BottomNavigation value={pathname} onChange={(e, newRoute) => history.push(newRoute)} showLabels>
+      <BottomNavigationAction value='/' label='Matches' icon={<GolfCourse />} />
+      <BottomNavigationAction value='/settings' label='Settings' icon={<Settings />} />
     </BottomNavigation>
   );
 } 
