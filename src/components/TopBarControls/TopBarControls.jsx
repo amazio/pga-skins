c683adb7cacdd7ae924fdcf43aa301b6524d0b2d
amazio <amaziosoftware@gmail.com>
2021-02-11 14:29:11 -0800

    Fix mobile window height

--- src/components/TopBarControls/TopBarControls.jsx
+++ src/components/TopBarControls/TopBarControls.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useContext, useRef } from 'react';
+import React, { useState, useContext } from 'react';
 import { useHistory, useLocation } from 'react-router-dom';
 import { Button, Snackbar } from '@material-ui/core';
 import { Add } from '@material-ui/icons';
