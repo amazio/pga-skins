26fc56c0dffed88450e9283b1f07cb892aded73b
amazio <amaziosoftware@gmail.com>
2021-02-11 18:34:00 -0800

    Fix bug

--- socket.js
+++ socket.js
@@ -6,7 +6,7 @@ socket.on('connect', function() {
   console.log('Connected to socket.io server');
 });
 
-socket.on(messages.LB_UPDATED, function({tourney, updatedPlayerIds}) {
+socket.on(messages.LB_UPDATED, function({ tourney, updatedPlayerIds }) {
   console.log('Received updated tourney', tourney._id);
   console.log('Updated player Ids: ', updatedPlayerIds);
   realtimeService.updateCurrentTourney(tourney, updatedPlayerIds);
