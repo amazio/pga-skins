f6f9ced233a1978598d4b9248547cfc144f71fe2
amazio <amaziosoftware@gmail.com>
2021-02-12 08:40:36 -0800

    Ensure that savedMatches is updated when shared link is browsed to

--- src/reducers/store-reducer.js
+++ src/reducers/store-reducer.js
@@ -48,8 +48,8 @@ function storeReducer(state, action) {
       return {...state, formData: {...state.formData, ...action.payload}};
     case actions.UPDATE_VIEWING_MATCH:
       matchService.updateSavedMatch(action.payload);
-      const savedMatches = matchService.getSavedMatches();
-      return {...state, viewingMatch: action.payload, savedMatches: savedMatches};
+      const matches = matchService.getSavedMatches();
+      return {...state, viewingMatch: action.payload, savedMatches: matches};
     case actions.SET_ALL_MATCHES:
       matchService.setSavedMatches(action.payload);
       return {...state, savedMatches: action.payload};
